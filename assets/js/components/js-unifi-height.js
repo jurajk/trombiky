9a0f60bbf3b53baef8233f9875d5ba3f4adf869f
Andrzej <hello@andrzej.sk>
2017-11-20 19:19:07 +0100

    Fresh foundation sites template powered by twig

---
+++ assets/js/components/js-unifi-height.js
@@ -0,0 +1,27 @@
+(function($){
+    var $unifiChildHeight = $(".js-unifiChildHeight");
+    if($unifiChildHeight.length > 0 && $(window).width() > 800) {
+        $(window).resize(function() {
+            $unifiChildHeight.each(function(index, element) {
+                var $element = $(element);
+                if(!$element.is(":visible")) {
+                  return;
+                }
+                var maxHeight = 0;
+                var $unifiedElements = $element.find(".js-unifiedElement");
+                $unifiedElements.height("");
+                $unifiedElements.each(function(index, element){
+                  var elementHeight = $(element).height();
+                  if(elementHeight > maxHeight) {
+                    maxHeight = elementHeight;
+                  }
+                });
+                $unifiedElements.height(maxHeight+50);
+            });
+        });
+
+        setTimeout(function() {
+            $(window).trigger("resize");
+        }, 60);
+    }
+})(jQuery);
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+
+	/*
+	* SVG 4 everybody
+	*/
+
+	svg4everybody({
+	    polyfill: true // polyfill <use> elements for External Content
+	});
+
+
+});
