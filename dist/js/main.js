d4dc3e8050f97597da452ffed022f6bf2b2cdaf6
Andrzej <hello@andrzej.sk>
2017-12-28 11:59:06 +0100

    workaround - add ios-safari notice because of google forms iframe jumping issue we cannot influence

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,7 +10,7 @@ var $target=$(target);$target.focus();if($target.is(":focus")){// Checking if th
 return false;}else{$target.attr('tabindex','-1');// Adding tabindex for elements not focusable
 $target.focus();// Set focus again
 };});}}});});
-"use strict";(function($){// $(document).foundation();
+'use strict';(function($){// $(document).foundation();
 })(jQuery);jQuery(document).ready(function($){/*
 	* Initialize Foundation Sites plugins
 	*/// $(document).foundation();
@@ -19,4 +19,8 @@ $target.focus();// Set focus again
 	*/// svg4everybody({
 //     polyfill: true // polyfill <use> elements for External Content
 // });
-});
+// Try to detect devices with form jumpy behavior
+var iosClass='js-ios-detected';var md=new MobileDetect(window.navigator.userAgent);if(md.userAgent()&&md.os()){console.log(md.userAgent());// looking for 'Safari'
+console.log(md.os());// looking for 'iOS'
+if(md.userAgent()==='Safari'&&md.os()==='iOS'){$('.js-body').addClass(iosClass);}}//button to show form anyway on an iOs device
+$('.js-show-form').on('click touch',function(e){console.log('show form');$('.js-body').removeClass(iosClass);});});
--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,8 @@ var paths = {
         ],
         copyScripts: [
         'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/svg4everybody/dist/svg4everybody.min.js'
+        'node_modules/svg4everybody/dist/svg4everybody.min.js',
+        'node_modules/mobile-detect/mobile-detect.min.js'
         ],
         images: ['assets/svg/*.svg'],
         copyImages: [
